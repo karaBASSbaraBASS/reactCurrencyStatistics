34fd07677f7a986157ce68a7529d7804b3eb5bc6
Jack <evgen.mikhalov@gmail.com>
2019-04-19 02:05:30 +0300

    routing fix

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { HashRouter, Route, Switch } from 'react-router-dom';
 import Header from './components/common/Header';
 import List from './components/list/List';
 import NotFound from './components/notFound/NotFound';
@@ -9,17 +9,17 @@ import './index.css';
 
 const App = () => {
     return (
-        <BrowserRouter>
+        <HashRouter basename='/'>
             <div>
                 <Header/>
                 
                 <Switch>
-                    <Route path={process.env.PUBLIC_URL+'/'} component={List} exact/>
-                    <Route path={process.env.PUBLIC_URL+'reactCurrencyStatistics/currency/:id'} component={Detail} exact/>
+                    <Route path='/' component={List} exact/>
+                    <Route path='/currency/:id' component={Detail} exact/>
                     <Route component={NotFound}/>
                 </Switch>
             </div>
-        </BrowserRouter>
+        </HashRouter>
     )
 }
 export default App;
